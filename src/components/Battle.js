377c87d0f553f0decfee965c2a1541592efadea3
CodingBae <e.brajan.98@gmail.com>
2019-05-21 00:14:08 +0200

    added scripts for battle functionality

--- src/components/Battle.js
+++ src/components/Battle.js
@@ -1,10 +1,143 @@
 import React from "react";
+import "./Battle.css";
+import { Link } from "react-router-dom";
+import PlayerPreview from "./PlayerPreview";
+//The following component {PlayerInput} is unique to this component
+// therefore no new file was created
 
-class Battle extends React.Component {
+class PlayerInput extends React.Component {
+  state = {
+    username: ""
+  };
+
+  handleChange = event => {
+    var value = event.target.value;
+    this.setState(() => {
+      return { username: value };
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.props.id, this.state.username);
+  };
+
   render() {
     return (
+      <form className="column" onSubmit={this.handleSubmit}>
+        <label className="header" htmlFor="username">
+          {this.props.label}
+        </label>
+        <input
+          id="username"
+          placeholder="github username"
+          type="text"
+          autoComplete="off"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <button
+          className="button"
+          type="submit"
+          disabled={!this.state.username}
+        >
+          Enter the battle!
+        </button>
+      </form>
+    );
+  }
+}
+
+class Battle extends React.Component {
+  state = {
+    playerOneName: "",
+    playerTwoName: "",
+    playerOneImage: null,
+    playerTwoImage: null
+  };
+
+  handleSubmit = (id, username) => {
+    this.setState(() => {
+      var newState = {};
+      newState[id + "Name"] = username;
+      newState[id + "Image"] =
+        "https://github.com/" + username + ".png?size=200";
+      return newState;
+    });
+  };
+
+  handleReset = id => {
+    this.setState(() => {
+      var newState = {};
+      newState[id + "Name"] = "";
+      newState[id + "Image"] = null;
+      return newState;
+    });
+  };
+
+  render() {
+    var match = this.props.match;
+    var playerOneName = this.state.playerOneName;
+    var playerTwoName = this.state.playerTwoName;
+    var playerOneImage = this.state.playerOneImage;
+    var playerTwoImage = this.state.playerTwoImage;
+
+    return (
       <div>
-        <h1>BATTLE</h1>
+        <div className="row">
+          {!playerOneName && (
+            <PlayerInput
+              id="playerOne"
+              label="Player One"
+              onSubmit={this.handleSubmit}
+            />
+          )}
+
+          {playerOneImage !== null && (
+            <PlayerPreview avatar={playerOneImage} username={playerOneName}>
+              <button
+                className="reset"
+                onClick={this.handleReset.bind(null, "playerOne")}
+              >
+                Reset
+              </button>
+            </PlayerPreview>
+          )}
+
+          {!playerTwoName && (
+            <PlayerInput
+              id="playerTwo"
+              label="Player Two"
+              onSubmit={this.handleSubmit}
+            />
+          )}
+
+          {playerTwoImage !== null && (
+            <PlayerPreview avatar={playerTwoImage} username={playerTwoName}>
+              <button
+                className="reset"
+                onClick={this.handleReset.bind(null, "playerTwo")}
+              >
+                Reset
+              </button>
+            </PlayerPreview>
+          )}
+        </div>
+        {playerOneImage && playerTwoImage && (
+          <Link
+            className="button"
+            to={{
+              pathname: match.url + "/results",
+              search:
+                "?playerOneName=" +
+                playerOneName +
+                "&playerTwoName=" +
+                playerTwoName
+            }}
+          >
+            Battle
+          </Link>
+        )}
       </div>
     );
   }
