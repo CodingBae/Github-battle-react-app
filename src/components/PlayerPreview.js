eeee05461c0ee4bf76ea07a21ef1029e452f0a97
CodingBae <e.brajan.98@gmail.com>
2019-05-21 00:12:26 +0200

    added scripts for battle functionality

---
+++ src/components/PlayerPreview.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function PlayerPreview(props) {
+  return (
+    <div>
+      <div className="column">
+        <img src={props.avatar} alt={"Avatar for " + props.username} />
+        <h2 className="username">@{props.username}</h2>
+      </div>
+      {props.children}
+    </div>
+  );
+}
+export default PlayerPreview;
---
+++ src/components/Results.js
@@ -0,0 +1,96 @@
+import React from "react";
+import queryString from "query-string";
+import api from "../utils/api";
+import { Link } from "react-router-dom";
+import PlayerPreview from "./PlayerPreview";
+
+function Profile(props) {
+  var info = props.info;
+  return (
+    <PlayerPreview avatar={info.avatar_url} username={info.login}>
+      <ul className="space-list-items">
+        {info.name && <li>{info.name}</li>}
+        {info.location && <li>{info.location}</li>}
+        {info.company && <li>{info.company}</li>}
+        <li>Followers : {info.followers}</li>
+        <li>Following : {info.following}</li>
+        <li>Public Repos : {info.public_repos}</li>
+        {info.blog && (
+          <li>
+            <a href={info.blog}>{info.blog}</a>
+          </li>
+        )}
+      </ul>
+    </PlayerPreview>
+  );
+}
+
+function Player(props) {
+  return (
+    <div>
+      <h1 className="header">{props.label}</h1>
+      <h3>Score:{props.score}</h3>
+      <Profile info={props.profile} />
+    </div>
+  );
+}
+
+class Results extends React.Component {
+  state = {
+    winner: null,
+    loser: null,
+    error: null,
+    loading: true
+  };
+
+  componentDidMount() {
+    var players = queryString.parse(this.props.location.search);
+    api.battle([players.playerOneName, players.playerTwoName]).then(results => {
+      if (results === null) {
+        return this.setState(() => {
+          return {
+            error:
+              "Hey buddy there is some error, check for any typos and try again....",
+            loading: false
+          };
+        });
+      }
+      this.setState(() => {
+        return {
+          error: null,
+          winner: results[0],
+          loser: results[1],
+          loading: false
+        };
+      });
+    });
+  }
+  render() {
+    var error = this.state.error;
+    var winner = this.state.winner;
+    var loser = this.state.loser;
+    var loading = this.state.loading;
+
+    if (loading === true) {
+      return <h1>LOADING......</h1>;
+    }
+
+    if (error) {
+      return (
+        <div>
+          <h1>{error}</h1>
+          <Link to="/battle">Try Again</Link>
+        </div>
+      );
+    }
+
+    return (
+      <div className="row">
+        <Player label="Winner" score={winner.score} profile={winner.profile} />
+        <Player label="Loser" score={loser.score} profile={loser.profile} />
+      </div>
+    );
+  }
+}
+
+export default Results;
